6560eff41869c7796507873bdbd31c115b7ca05b
Aaron Oppenheimer <aaron.oppenheimer@cfa.harvard.edu>
2022-02-25 15:46:08 -0500

    added more error checks

--- ngehtutil/source/sources.py
+++ ngehtutil/source/sources.py
@@ -1,6 +1,7 @@
 """
 Manage things to do with sources
 """
+from multiprocessing.sharedctypes import Value
 from pathlib import Path
 import pandas as pd
 from PIL import Image
@@ -25,14 +26,20 @@ def get_source_list():
 
 
 def get_source_info(source):
+    if not source in THE_SOURCES.index:
+        raise ValueError(f'No source {source}')
     return dict(THE_SOURCES.loc[source])
 
 
 def get_source_description(source):
+    if not source in THE_SOURCES.index:
+        raise ValueError(f'No source {source}')
     return THE_SOURCES.loc[source,'DESCRIPTION']
 
 
 def get_source_freq_list(source):
+    if not source in THE_SOURCES.index:
+        raise ValueError(f'No source {source}')
     the_list=[]
     for freq in [86, 230, 345, 480, 690]:
         if type(THE_SOURCES.loc[source][f'{freq}_DATA']) is str:
@@ -42,6 +49,12 @@ def get_source_freq_list(source):
 
 def get_source_image(source, frequency):
     """ load the image file for a given source and frequency """
+    if not source in THE_SOURCES.index:
+        raise ValueError(f'No source {source}')
+
+    if not frequency in get_source_freq_list(source):
+        raise ValueError(f'No frequency {frequency} for source {source}')
+
     file_name = THE_SOURCES.loc[source][f'{int(frequency)}_IMAGE']
     if file_name:
         path = os.path.abspath(__file__)
@@ -54,6 +67,12 @@ def get_source_image(source, frequency):
 
 def get_source_data_file(source, frequency):
     """ return the data file for a given source and frequency """
+    if not source in THE_SOURCES.index:
+        raise ValueError(f'No source {source}')
+
+    if not frequency in THE_SOURCES.loc[source]:
+        raise ValueError(f'No frequency {frequency} for source {source}')
+
     file_name = THE_SOURCES.loc[source][f'{int(frequency)}_DATA']
     if file_name:
         path = os.path.abspath(__file__)
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,9 +1,14 @@
 # test arrays
+from multiprocessing.sharedctypes import Value
 from ngehtutil.source import get_source_description, get_source_list, \
-    get_default_source, get_source_info, get_source_image
+    get_source_info, get_source_image
 import unittest
 import PIL
-import numpy
+
+
+def get_default_source():
+    return get_source_list()[0]
+
 
 class TestClass(unittest.TestCase):
     def test_source_list(self):
@@ -11,20 +16,31 @@ class TestClass(unittest.TestCase):
         self.assertEqual(type(a),list)
         self.assertTrue(len(a)>0)
 
-    def test_default_source(self):
-        d = get_default_source()
-        self.assertEqual(type(d),str)
 
     def test_source_info(self):
         sl = get_source_list()
         info = get_source_info(sl[0])
         self.assertEqual(type(info),dict)
 
+        with self.assertRaises(ValueError):
+            info = get_source_info('aaron')
+
+
     def test_source_description(self):
         sl = get_source_list()
         desc = get_source_description(sl[0])
         self.assertEqual(type(desc),str)
 
+        with self.assertRaises(ValueError):
+            info = get_source_description('aaron')
+
+
     def test_source_image(self):
         im = get_source_image(get_default_source(),230)
         self.assertEqual(type(im), PIL.PngImagePlugin.PngImageFile)
+
+        with self.assertRaises(ValueError):
+            info = get_source_image('aaron',230)
+
+        with self.assertRaises(ValueError):
+            info = get_source_image('M87',999)
