640e1f32c8db9a87ec18a79871b405d031a4dc70
Aaron Oppenheimer <aaron.oppenheimer@cfa.harvard.edu>
2022-03-01 10:36:23 -0500

    updated costs over time code

--- ngehtutil/cost/cost_timing.py
+++ ngehtutil/cost/cost_timing.py
@@ -8,7 +8,7 @@ from math import floor
 import pandas as pd
 import logging
 
-def calculate_costs_over_time(cost_config, array_name, array_costs):
+def calculate_costs_over_time(cost_config, array_costs):
     """ Do the calculation of array costs for each of the arrays described in array_data """
     costs_over_time = pd.DataFrame(dtype=float)
 
@@ -17,10 +17,10 @@ def calculate_costs_over_time(cost_config, array_name, array_costs):
 
     logging.info(f'calculating costs over time')
 
-    years_to_build = cost_config['fully_operational'] - cost_config['start_building'] + years_advance_spend
-    year_first_build = cost_config['start_building'] - years_advance_spend
-    final_year = cost_config['fully_operational'] + cost_config['active_lifetime']
-    inflation = 1 + cost_config['inflation_rate']
+    years_to_build = cost_config.fully_operational - cost_config.start_building + years_advance_spend
+    year_first_build = cost_config.start_building - years_advance_spend
+    final_year = cost_config.fully_operational + cost_config.active_lifetime
+    inflation = 1 + cost_config.inflation_rate
     average_annual_capex = array_costs.loc['TOTAL CAPEX'] / years_to_build
     average_annual_opex = array_costs.loc['ANNUAL OPEX'] / array_costs.loc['Total Sites Count']
 
@@ -29,14 +29,14 @@ def calculate_costs_over_time(cost_config, array_name, array_costs):
     stations_built_per_year = stations_to_build / years_to_build
     stations_built = 0
     for year in range(calculation_start, final_year+1):
-        building_in_progress = year >= year_first_build and year <= cost_config['fully_operational']
-        stations_operating = year >= cost_config['start_building']
+        building_in_progress = year >= year_first_build and year <= cost_config.fully_operational
+        stations_operating = year >= cost_config.start_building
 
         cap_spend = (average_annual_capex * pow(inflation, year-calculation_start)) \
             if building_in_progress else 0
 
         stations_built += stations_built_per_year \
-            if building_in_progress and year < cost_config['fully_operational'] else 0
+            if building_in_progress and year < cost_config.fully_operational else 0
 
         stations_available = existing_stations + floor(stations_built)
 
@@ -44,7 +44,7 @@ def calculate_costs_over_time(cost_config, array_name, array_costs):
                     pow(inflation, year - calculation_start)) \
                         if stations_operating else 0
 
-        costs_over_time.loc[year,f'{array_name} - CAPEX'] = float(cap_spend)
-        costs_over_time.loc[year,f'{array_name} - OPEX'] = float(op_spend)
+        costs_over_time.loc[year,'CAPEX'] = float(cap_spend)
+        costs_over_time.loc[year,'OPEX'] = float(op_spend)
 
     return costs_over_time
