8e94425a86f323bef89862478817fb8864f98f9d
Aaron Oppenheimer <aaron.oppenheimer@cfa.harvard.edu>
2022-03-18 12:03:31 -0400

    moved source to object

--- ngehtutil/source/sources.py
+++ ngehtutil/source/sources.py
@@ -9,6 +9,63 @@ import os
 
 THE_SOURCES = None
 
+
+class Source:
+
+    name = None
+    
+    @staticmethod
+    def get_source_list():
+        return list(THE_SOURCES.keys())
+
+    @staticmethod
+    def get(name):
+        return THE_SOURCES[name]
+
+    def __init__(self, name, **kwargs):
+        self.name = name
+        for k,v in kwargs.items():
+            setattr(self, k.lower(), v)
+
+    def freq_list(self):
+        the_list=[]
+        for freq in [86, 230, 345, 480, 690]:
+            if type(getattr(self,f'{freq}_data',None)) is str:
+                the_list.append(freq)
+        return the_list
+
+
+    def picture(self, frequency):
+        """ load the image file for a given source and frequency """
+
+        if not frequency in self.freq_list():
+            raise ValueError(f'No frequency {frequency} for source {self.name}')
+
+        file_name = getattr(self,f'{int(frequency)}_image')
+        if file_name:
+            path = os.path.abspath(__file__)
+            dir_path = os.path.dirname(path)
+            im = Image.open(f'{dir_path}/models/{file_name}')
+            return im
+        else:
+            return None
+
+
+    def fits(self, frequency):
+        """ return the data file for a given source and frequency """
+        if not frequency in self.freq_list():
+            raise ValueError(f'No frequency {frequency} for source {self.name}')
+
+        file_name = getattr(self, f'{int(frequency)}_data')
+        if file_name:
+            path = os.path.abspath(__file__)
+            dir_path = os.path.dirname(path)
+            data = f'{dir_path}/models/{file_name}'
+            return data
+        else:
+            return None
+
+
  ## Helper functions
 
 def _init_sources():
@@ -17,67 +74,7 @@ def _init_sources():
     if THE_SOURCES is None:
         path=str(Path(__file__).parent) + '/config'
         srcs = pd.read_csv(f'{path}/sources.csv', index_col=0)
-        THE_SOURCES = srcs
+        THE_SOURCES = {x:Source(name=x, **(srcs.loc[x].to_dict())) for x in srcs.index}
 
 _init_sources()
 
-def get_source_list():
-    return list(THE_SOURCES.index)
-
-
-def get_source_info(source):
-    if not source in THE_SOURCES.index:
-        raise ValueError(f'No source {source}')
-    return dict(THE_SOURCES.loc[source])
-
-
-def get_source_description(source):
-    if not source in THE_SOURCES.index:
-        raise ValueError(f'No source {source}')
-    return THE_SOURCES.loc[source,'DESCRIPTION']
-
-
-def get_source_freq_list(source):
-    if not source in THE_SOURCES.index:
-        raise ValueError(f'No source {source}')
-    the_list=[]
-    for freq in [86, 230, 345, 480, 690]:
-        if type(THE_SOURCES.loc[source][f'{freq}_DATA']) is str:
-            the_list.append(freq)
-    return the_list
-
-
-def get_source_image(source, frequency):
-    """ load the image file for a given source and frequency """
-    if not source in THE_SOURCES.index:
-        raise ValueError(f'No source {source}')
-
-    if not frequency in get_source_freq_list(source):
-        raise ValueError(f'No frequency {frequency} for source {source}')
-
-    file_name = THE_SOURCES.loc[source][f'{int(frequency)}_IMAGE']
-    if file_name:
-        path = os.path.abspath(__file__)
-        dir_path = os.path.dirname(path)
-        im = Image.open(f'{dir_path}/models/{file_name}')
-        return im
-    else:
-        return None
-
-
-def get_source_data_file(source, frequency):
-    """ return the data file for a given source and frequency """
-    if not source in THE_SOURCES.index:
-        raise ValueError(f'No source {source}')
-
-    if not frequency in get_source_freq_list(source):
-        raise ValueError(f'No frequency {frequency} for source {source}')
-
-    file_name = THE_SOURCES.loc[source][f'{int(frequency)}_DATA']
-    if file_name:
-        path = os.path.abspath(__file__)
-        dir_path = os.path.dirname(path)
-        data = f'{dir_path}/models/{file_name}'
-        return data
-    else:
-        return None
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,58 +1,52 @@
 # test arrays
 from multiprocessing.sharedctypes import Value
-from ngehtutil.source import get_source_description, get_source_list, \
-    get_source_info, get_source_image, get_source_data_file
+from ngehtutil.source import Source
 import unittest
 import PIL
 
 
 def get_default_source():
-    return get_source_list()[0]
-
+    return Source.get_source_list()[0]
 
 class TestClass(unittest.TestCase):
     def test_source_list(self):
-        a = get_source_list()
+        a = Source.get_source_list()
         self.assertEqual(type(a),list)
         self.assertTrue(len(a)>0)
 
 
     def test_source_info(self):
-        sl = get_source_list()
-        info = get_source_info(sl[0])
-        self.assertEqual(type(info),dict)
+        sl = Source.get_source_list()
+        info = Source.get(sl[0])
+        self.assertEqual(type(info),Source)
 
-        with self.assertRaises(ValueError):
-            info = get_source_info('aaron')
+        with self.assertRaises(KeyError):
+            info = Source.get('aaron')
 
 
     def test_source_description(self):
-        sl = get_source_list()
-        desc = get_source_description(sl[0])
+        sl = Source.get_source_list()
+        s = Source.get(sl[0])
+        desc = s.description
         self.assertEqual(type(desc),str)
-
-        with self.assertRaises(ValueError):
-            info = get_source_description('aaron')
 
 
     def test_source_image(self):
-        im = get_source_image(get_default_source(),230)
+        sl = Source.get_source_list()
+        s = Source.get(sl[0])
+        im = s.picture(230)
         self.assertEqual(type(im), PIL.PngImagePlugin.PngImageFile)
 
         with self.assertRaises(ValueError):
-            info = get_source_image('aaron',230)
-
-        with self.assertRaises(ValueError):
-            info = get_source_image('M87',999)
+            info = s.picture(999)
 
 
     def test_source_data_file(self):
-        f = get_source_data_file(get_default_source(),230)
+        sl = Source.get_source_list()
+        s = Source.get(sl[0])
+
+        f = s.fits(230)
         self.assertEqual(type(f), str)
 
         with self.assertRaises(ValueError):
-            f = get_source_data_file('aaron',230)
-
-        with self.assertRaises(ValueError):
-            f = get_source_data_file('M87',999)
-
+            f = s.fits(999)
