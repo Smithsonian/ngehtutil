ac7770c7c2b475ab90a093b6bcb9c5f9e2bcfcd4
Aaron Oppenheimer <aaron.oppenheimer@cfa.harvard.edu>
2022-08-01 14:08:16 -0400

    linting

--- ngehtutil/__init__.py
+++ ngehtutil/__init__.py
@@ -6,11 +6,11 @@ the root directory of this distribution.
 
 Originator: Aaron Oppenheimer March 2020
 """
-from .target import *
-from .source import *
-from .cost import *
-from .campaign import *
-from .station import *
-from .array import *
-from .program import *
-from .version import VERSION
+from .target import *  # noqa F401
+from .source import *  # noqa F401
+from .cost import *  # noqa F401
+from .campaign import *  # noqa F401
+from .station import *  # noqa F401
+from .array import *  # noqa F401
+from .program import *  # noqa F401
+from .version import VERSION  # noqa F401
