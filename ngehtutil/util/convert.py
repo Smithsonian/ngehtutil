8db6c956c7fbcc4b255251370d1fc1831a3a51b2
Aaron Oppenheimer <aaron.oppenheimer@cfa.harvard.edu>
2022-08-01 13:57:47 -0400

    linting

--- ngehtutil/util/convert.py
+++ ngehtutil/util/convert.py
@@ -2,15 +2,15 @@ import ehtim as eh
 
 
 files = [
-'avery_m87_1_eofn',
-'avery_sgra_eofn',
-'jason_mad_eofn',
-'roman_eofn',
-'avery_m87_2_eofn',
-'howes_m87',
-'jason_mri_eofn',
-'rowan_m87'
-]
+            'avery_m87_1_eofn',
+            'avery_sgra_eofn',
+            'jason_mad_eofn',
+            'roman_eofn',
+            'avery_m87_2_eofn',
+            'howes_m87',
+            'jason_mri_eofn',
+            'rowan_m87'
+            ]
 
 for f in files:
     im = eh.image.load_txt(f'models/{f}.txt')
